154699dcc8ef5bbd5b331d8cfa198ef3c6c6dabc
Mateusz Bogolubow <mateusz@bogolubow.com>
2021-01-06 13:48:20 +0100

    Change text from React to Vue

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with devmentor.pl
+          Learn Vue with devmentor.pl
         </a>
 
       </header>
